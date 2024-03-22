56321f54c85eb4b4005b97e799b4f8e6d636806c
stefanoZS <stefano.trevisan@zilioservice.com>
2024-03-22 15:43:22 +0100

    Versione beta

--- main.py
+++ main.py
@@ -8,16 +8,11 @@ import telebot
 def calcola_dt():
 
     Now = datetime.now()
-    currMonth = Now.month
-    currYear = Now.year
-    nextMonth = currMonth % 12 + 1
 
-    if nextMonth != 1:
-        nextMonthYear = currYear
-    else:
-        nextMonthYear = currYear + 1
+    starting_scantime = datetime(Now.year, Now.month, Now.day, 8, 30)
 
-    starting_scantime = datetime(nextMonthYear, nextMonth, 1, 8, 30)
+    if Now.weekday() == 0:
+        starting_scantime = Now + timedelta(days=1)
     weekDay = starting_scantime.weekday()
 
     while weekDay != 0:
@@ -55,33 +50,58 @@ def calcola_dt():
         Mese = "novembre"
     elif month == 12:
         Mese = "dicembre"
+    else:
+        Mese = ""
 
     dataString = ("Il prossimo alert arriverà lunedì " + str(stopping_scantime.day) + " " + Mese + " "
                   + str(stopping_scantime.year))
 
-    return delta_t, dataString
+    return stopping_scantime, dataString, delta_t
 
 
-def main():
-
+def trovaFile():
     # trova il nome del file
-    os.chdir("Z:")
+    os.chdir("Q:")
     Dir = os.listdir()
 
     found = 0
 
     i = 0
-    FileName = []
+    ScadenzarioFileName = []
 
     while found == 0:
 
         if Dir[i][0:21] == "Scadenzario polizze_2":
-            FileName = Dir[i]
+            ScadenzarioFileName = Dir[i]
             found = 1
         else:
             i = i + 1
 
-    Tab = pd.read_excel(FileName)
+    return ScadenzarioFileName
+
+
+def main():
+
+    # trova il nome del file
+
+    os.chdir("Q:")
+    Dir = os.listdir()
+
+    found = 0
+
+    i = 0
+    ScadenzarioFileName = []
+
+    while found == 0:
+
+        if Dir[i][0:21] == "Scadenzario polizze_2":
+            ScadenzarioFileName = Dir[i]
+            found = 1
+
+        else:
+            i = i + 1
+
+    Tab = pd.read_excel(ScadenzarioFileName)
     Tab["SCADENZA"] = pd.to_datetime(Tab["SCADENZA"], dayfirst=True)
     Now = datetime.now()
     dt_2mesi = timedelta(days=60)
@@ -92,7 +112,6 @@ def main():
     SelTab = Tab[Tab["SCADENZA"] <= deadline]
 
     # riordino la tabella in base alla data
-    SortTab = SelTab.sort_values(by=["SCADENZA"], ascending=False)
 
     Scadenze = SelTab["SCADENZA"]
     timesLeft = Now - Scadenze
@@ -107,11 +126,11 @@ def main():
 
     text = "*OCCHIO alle scadenze!*\n \n"
 
-
     for i in range(len(firstTab)):
 
         Scadenza = firstTab["SCADENZA"].iloc[i]
         Polizza = firstTab["Tipologia polizza"].iloc[i]
+        Riferimento = firstTab["Riferimento"].iloc[i]
 
         timeLeft = Now - Scadenza
         timeLeft = timeLeft.days
@@ -126,13 +145,18 @@ def main():
         ScadenzaString = datetime.strftime(Scadenza, "%d/%m/%Y")
         Compagnia = firstTab["Compagnia assicurativa"].iloc[i]
         string = (sign+str(timeLeft) + " " + symbol + " Polizza "+Polizza + " " + Compagnia + " in scadenza il " +
-                  ScadenzaString + "\n \n")
+                  ScadenzaString + " - " + str(Riferimento) + "\n \n")
         text = text + string
 
     for i in range(len(SecondTab)):
 
         Scadenza = SecondTab["SCADENZA"].iloc[i]
         Polizza = SecondTab["Tipologia polizza"].iloc[i]
+        Nota = str(SecondTab["NOTE"].iloc[i])
+        Riferimento = firstTab["Riferimento"].iloc[i]
+
+        if Nota == "nan":
+            Nota = ""
 
         timeLeft = Now - Scadenza
         timeLeft = timeLeft.days
@@ -146,11 +170,11 @@ def main():
 
         ScadenzaString = datetime.strftime(Scadenza, "%d/%m/%Y")
         Compagnia = SecondTab["Compagnia assicurativa"].iloc[i]
-        string = (sign+str(timeLeft) + " " + symbol + " Polizza "+Polizza + " " + Compagnia + " in scadenza il " +
-                  ScadenzaString + "\n \n")
+        string = (sign+str(timeLeft) + " " + str(Nota) + " " + symbol + " Polizza "+Polizza + " " + Compagnia +
+                  " scaduta il " + ScadenzaString + " - " + Riferimento + "\n \n")
         text = text + string
 
-    dt, dataString = calcola_dt()
+    t, dataString, tLeft = calcola_dt()
     text = text + dataString
 
     token = "6007635672:AAF_kA2nV4mrscssVRHW0Fgzsx0DjeZQIHU"
@@ -159,10 +183,35 @@ def main():
     run_id = "-1001995962404"
     bot.send_message(test_id, text=text, parse_mode='Markdown')
 
-    return dt
+    return tLeft, t
 
+
+now = datetime.now()
+NextSend = now - timedelta(hours=1)
 
 while True:
-    dt = main()
-    print("Prossimo alert tra: "+str(round(dt/3600/24)) + " giorni.")
+
+    curr_dir = os.getcwd()
+    now = datetime.now()
+    last_modify = pd.read_csv("last_modify.csv")
+    last_modify = last_modify["last_modify"][0]
+    last_modify = pd.to_datetime(last_modify)
+    # last_modify = datetime.hrtime(last_modify)
+    FileName = trovaFile()
+    modify_time = os.path.getmtime("Q:"+FileName)
+    modify_time = datetime.fromtimestamp(modify_time)
+
+    # modify_time = last_modify[0]
+
+    if modify_time > last_modify or now > NextSend:
+        dtLeft, NextSend = main()
+        modify_time_dict = {"last_modify": modify_time}
+        modify_time_df = pd.DataFrame(modify_time_dict, index=[0])
+        os.chdir(curr_dir)
+        modify_time_df.to_csv("last_modify.csv", index=False)
+        print("Prossimo alert tra: "+str(round(dtLeft/3600/24)) + " giorni.")
+
+    os.chdir(curr_dir)
+
+    dt = 60
     time.sleep(dt)
